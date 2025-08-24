7e997d3cf945639ac8d252400f0225ccabf71940
Tanish2win <tanish04ram@gmail.com>
2025-08-25 00:07:27 +0530

    Update and rename layout.js to layout.tsx

--- LINE_FOLLOWING_CAR/app/layout.tsx
+++ LINE_FOLLOWING_CAR/app/layout.tsx
@@ -1,9 +1,11 @@
+import React, { ReactNode } from "react";
+
 export const metadata = {
   title: "Line Following Car",
   description: "Testing website for Line Following Car project",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body style={{ margin: 0, fontFamily: "Arial, sans-serif" }}>
